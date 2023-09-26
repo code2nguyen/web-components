45417e31655fe69ff97ab1c9045ab0d5fa060e95
Nguyen Thai Vinh <code2nguyen@gmail.com>
2023-09-26 16:29:06 +0200

    update astro config

--- demo/astro.config.mjs
+++ demo/astro.config.mjs
@@ -5,6 +5,8 @@ import lit from '@astrojs/lit';
 import { MDXCodeBlockRemark } from './plugin/mdx-codeblock-remark.mjs';
 
 export default defineConfig({
+  site: 'https://code2nguyen.github.io',
+  base: '/web-components',
   markdown: {
     remarkPlugins: [MDXCodeBlockRemark]
   },
