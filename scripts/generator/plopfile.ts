4f0231c6003d8be189fe51c153eed61f09066333
Nguyen Thai Vinh <code2nguyen@gmail.com>
2025-01-24 23:08:21 +0100

    add template open package

--- scripts/generator/plopfile.ts
+++ scripts/generator/plopfile.ts
@@ -9,40 +9,58 @@ export default function (plop: NodePlopAPI) {
         name: 'name',
         message: 'wc name(ex: Dropdown List):',
       },
-    ],
-    actions: [
       {
-        type: 'addMany',
-        destination: '../../packages/{{dashCase name}}',
-        base: 'files/wc',
-        templateFiles: 'files/wc/**/*.*',
-        skipIfExists: true,
-      },
-      {
-        type: 'add',
-        path: '../../demo/src/content/components/{{dashCase name}}.mdx',
-        templateFile: 'files/demo-doc.mdx.hbs',
-        skipIfExists: true,
-      },
-      {
-        type: 'append',
-        path: '../../demo/src/store/component-manifests.ts',
-        pattern: /\n/,
-        separator: '',
-        template: "import {{ camelCase name }} from '@c2n/{{ dashCase name }}/custom-elements.json'\n",
-      },
-      {
-        type: 'append',
-        path: '../../demo/src/store/component-manifests.ts',
-        pattern: /const nomalizedManifests: ComponentManifests = \[/,
-        template: '    {{ camelCase name }},',
-      },
-      {
-        type: 'append',
-        path: '../../package.json',
-        pattern: /build.*\n?.*dependencies": \[/,
-        template: '        "./packages/{{ dashCase name }}:build",',
+        type: 'list',
+        name: 'package_type',
+        message: 'Select package type',
+        choices: ['npm package', 'open package'],
       },
     ],
+    actions: function (data) {
+      const { package_type } = data as { package_type: string }
+      const packages = package_type === 'npm package' ? 'packages' : 'open-packages'
+      const demo_content_folder = package_type === 'npm package' ? 'components' : 'oepn-components'
+      const actions =
+        package_type === 'npm package'
+          ? [
+              {
+                type: 'append',
+                path: '../../demo/src/store/component-manifests.ts',
+                pattern: /\n/,
+                separator: '',
+                template: "import {{ camelCase name }} from '@c2n/{{ dashCase name }}/custom-elements.json'\n",
+              },
+              {
+                type: 'append',
+                path: '../../demo/src/store/component-manifests.ts',
+                pattern: /const normalizedManifests: ComponentManifests = \[/,
+                template: '    {{ camelCase name }},',
+              },
+            ]
+          : []
+      return [
+        ...actions,
+        {
+          type: 'addMany',
+          destination: `../../${packages}/{{dashCase name}}`,
+          base: 'files/wc',
+          templateFiles: 'files/wc/**/*.*',
+          skipIfExists: true,
+        },
+        {
+          type: 'add',
+          path: `../../demo/src/content/${demo_content_folder}/{{dashCase name}}.mdx`,
+          templateFile: 'files/demo-doc.mdx.hbs',
+          skipIfExists: true,
+        },
+
+        {
+          type: 'append',
+          path: '../../package.json',
+          pattern: /build.*\n?.*dependencies": \[/,
+          template: `        "./${packages}/{{ dashCase name }}:build",`,
+        },
+      ]
+    },
   })
 }
